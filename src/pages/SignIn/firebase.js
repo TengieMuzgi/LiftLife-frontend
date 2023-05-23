24308818eee02929b5389f99a475a9f6c61e1306
mikkelx <michalss1115@gmail.com>
2023-05-24 00:13:24 +0200

    Firebase access file

---
+++ src/pages/SignIn/firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "variable_api_key",
+
+    authDomain: "realtime-database-prototype.firebaseapp.com",
+  
+    databaseURL: "https://realtime-database-prototype-default-rtdb.europe-west1.firebasedatabase.app",
+  
+    projectId: "realtime-database-prototype",
+  
+    storageBucket: "realtime-database-prototype.appspot.com",
+  
+    messagingSenderId: "variable_sender_id",
+  
+    appId: "variable_app_id"
+  
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth, signInWithEmailAndPassword };
